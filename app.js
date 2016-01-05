e69e0aa1f62c0a72f53b4f9172ac8fcd71097136
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-05 14:25:03 -0700

    add linked in route :zap:

--- app.js
+++ app.js
@@ -24,24 +24,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+require('dotenv').load()
 
 passport.use(new LinkedInStrategy({
-  clientID: '789ia4engv9ves',
-  clientSecret: 'boVAbX3SKwQYrP9A',
+  clientID: process.env.LINKEDIN_CLIENT_ID,
+  clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
 }, function(accessToken, refreshToken, profile, done) {
-  // asynchronous verification, for effect...
-  process.nextTick(function () {
-    // To keep the example simple, the user's LinkedIn profile is returned to
-    // represent the logged-in user. In a typical application, you would want
-    // to associate the LinkedIn account with a user record in your database,
-    // and return that user instead.
-    return done(null, profile);
-  });
-}));
+    done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
+  }
+));
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function(user, done) {
+  done(null, user)
+});
 
 app.use('/', routes);
 app.use('/users', users);
